94f4ee1c6eca34c9416c78d896b3725efc774a5e
RUFOS <odetokirufos@gmail.com>
2023-03-09 17:48:42 +0000

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,27 +14,30 @@ export class AppComponent implements OnInit{
 
 //initialisation avec cette fonction
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon enfant de toujours !',
-      'https://media.istockphoto.com/id/1325578537/fr/photo/heureuse-m%C3%A8re-indienne-samuser-avec-sa-fille-en-plein-air-concept-de-famille-et-damour.webp?s=612x612&w=is&k=20&c=mE534M7qi8h11KIEy3QoH2VTinCJUaDaiV5v2G98hn0=',
-      new Date(),
-      0
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Bath',
-      'EEEHOOO',
-      'http://www.religieux.org/wp-content/uploads/2018/09/Lire-ecouter-la-Bible-sur-le-net-pixabay-1280x765.jpg',
-      new Date(),
-      0
-    );
-    this.myLastSnap = new FaceSnap(
-      'Dorgeles',
-      'reiennnnnnnnnn',
-      'https://phoneky.co.uk/thumbs/android/thumbs/ico/8/s/louis-segond-android.jpg',
-      new Date(),
-      0
-    );
+    this.mySnap = {
+      title:'Archibald',
+      description:'Mon enfant de toujours !',
+      imageUrl:'https://media.istockphoto.com/id/1325578537/fr/photo/heureuse-m%C3%A8re-indienne-samuser-avec-sa-fille-en-plein-air-concept-de-famille-et-damour.webp?s=612x612&w=is&k=20&c=mE534M7qi8h11KIEy3QoH2VTinCJUaDaiV5v2G98hn0=',
+      createDate : new Date(),
+      snaps: 0,
+      location: 'Abidjan'
+    };
+    this.myOtherSnap = {
+      title: 'Bath',
+      description: 'EEEHOOO',
+      imageUrl:
+        'http://www.religieux.org/wp-content/uploads/2018/09/Lire-ecouter-la-Bible-sur-le-net-pixabay-1280x765.jpg',
+      createDate: new Date(),
+      snaps: 0,
+      location: 'Yakro',
+    };
+    this.myLastSnap = {
+      title:'Dorgeles',
+      description:'reiennnnnnnnnn',
+      imageUrl:'https://phoneky.co.uk/thumbs/android/thumbs/ico/8/s/louis-segond-android.jpg',
+      createDate : new Date(),
+      snaps : 0
+    };
 
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,11 +1,10 @@
 export class FaceSnap {
-  constructor(
-    public title: string,
-    public description: string,
-    public imageUrl: string,
-    public createdDate: Date,
-    public snaps: number
-  ) {}
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  location?: string;
 }
 
 
