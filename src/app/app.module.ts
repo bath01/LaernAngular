3a33cc9d5926cc0fe1cc6aba23aeee24c92efef8
RUFOS <odetokirufos@gmail.com>
2023-03-10 09:44:20 +0000

    forth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
@@ -7,12 +9,19 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent 
+    FaceSnapComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    //Pour la configuration de la langue en Francais
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
